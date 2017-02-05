fb9085ee6e267ce786856b8da5a1cbaf646380e6
Beau Dure <duresport@gmail.com>
2017-02-04 20:34:44 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions define something so complicated that we need to cache it, apparently
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+## Per Lester, the first two lines initialize our variables, and the "set" lines here do the same
+## in a new environment
+  
+  get <- function() x
+  setinvrs <- function(solve) m <<- solve
+  getinvrs <- function() m
+  list(set = set, get = get,
+       setinvrs = setinvrs,
+       getinvrs = getinvrs)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getinvrs()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinvrs(m)
+  m
 }
+
+## Gee, I wish I knew linear algebra
